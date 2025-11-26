59c45ba4be0a2c255997d032ae21343de6381c30
blckfrost <beataanima88@gmail.com>
2025-11-26 19:17:56 +0000

    dep setup

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,17 @@
 // @ts-check
+import tailwindcss from '@tailwindcss/vite';
 import { defineConfig } from 'astro/config';
 
+import vercel from '@astrojs/vercel';
+
+import react from '@astrojs/react';
+
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  vite: {
+      plugins: [tailwindcss()],
+  },
+
+  adapter: vercel(),
+  integrations: [react()],
+});
